c91475d32e473d8c784db0a89e95872f191bd256
Anthony Tam <40433247+tlapfai@users.noreply.github.com>
2021-10-25 13:57:58 +0800

    +cdr, vol

--- swpm/models.py
+++ swpm/models.py
@@ -13,6 +13,11 @@ FXO_CP = [("C","Call"), ("P","Put")]
 
 DAY_COUNTER = {"A360": ql.Actual360()}
 
+class Calendar(models.Model):
+    name = models.CharField(max_length=16)
+    def calendar(self):
+        return ql.TARGET()
+
 class Ccy(models.Model):
     code = models.CharField(max_length=3, blank=False)
     fixing_days = models.PositiveIntegerField()
@@ -20,30 +25,53 @@ class Ccy(models.Model):
         return self.code
 
 class CcyPair(models.Model):
-    base_ccy = models.ForeignKey(Ccy, CASCADE, related_name="base_ccy")
-    quote_ccy = models.ForeignKey(Ccy, CASCADE, related_name="quote_ccy")
+    base_ccy = models.ForeignKey(Ccy, CASCADE, related_name="as_base_ccy")
+    quote_ccy = models.ForeignKey(Ccy, CASCADE, related_name="as_quote_ccy")
+    cdr = models.ForeignKey(Calendar, DO_NOTHING, related_name="ccy_pair")
     def check_order():
         # check correct order
         return True
     def __str__(self):
         return f"{self.base_ccy}/{self.quote_ccy}"
+    def calendar(self):
+        return cdr.calendar()
     
 class RateIndex(models.Model):
-    pass
+    name = models.CharField(max_length=16)
 
 class RateQuote(models.Model):
+    name = models.CharField(max_length=16)
     rate = models.FloatField()
     tenor = models.CharField(max_length=5)
     type = models.CharField(max_length=5)
     ccy = models.ForeignKey(Ccy, CASCADE, related_name="rates")
     day_counter = models.CharField(max_length=5)
-    def rate_helper(self):
+    def helper(self):
         if type == "DEP":
             fixing_days = self.ccy.fixing_days
             convention = ql.ModifiedFollowing
             ccy = Ccy.objects.get(code=self.ccy)
             return ql.DepositRateHelper(self.rate, ql.Period(self.tenor), ccy.fixing_days, ql.TARGET(), convention, False, DAY_COUNTER[self.day_counter])
+    def __str__(self):
+        return f"{self.name}: {self.ccy}: {self.rate}"
 
+class IRTermStructure(models.Model):
+    name = models.CharField(max_length=16)
+    ref_date = models.DateField()
+    rates = models.ManyToManyField(RateQuote, related_name="ts")
+    def term_structure(self):
+        helpers = [rate.helper() for rate in rates]
+        return ql.PiecewiseLogLinearDiscount(ql.Date(self.ref_date.isoformat(),'%Y-%m-%d'), helpers, ql.Actual360())
+    def ccy(self):
+        return rates[0].ccy
+
+class FXVolatility(models.Model):
+    ref_date = models.DateField()
+    ccy_pair = models.ForeignKey(CcyPair, related_name='vol')
+    vol = models.FloatField()
+    def black_vol(self):
+        return ql.BlackConstantVol(ql.Date(self.ref_date.isoformat(),'%Y-%m-%d'), ccy_pair.calendar(), vol, ql.Actual365Fixed)
+    
 class FXOManager(models.Manager):
     def create_fxo(self, trade_date, maturity_date, ccypair, strike_price, type, cp, notional_1):
         fxo = self.create(
@@ -60,7 +88,6 @@ class FXOManager(models.Manager):
 
 
 class FXO(models.Model):
-
     active = models.BooleanField(default=True)
     create_time = models.DateTimeField(auto_now_add=True)
     trade_date = models.DateField(null=False)
@@ -76,7 +103,7 @@ class FXO(models.Model):
     def __str__(self):
         return f"FXO ID: {self.id}, {self.ccypair}, K={self.strike_price}"
     
-    def price(self):
+    def instrument(self):
         if self.active:
             cp = ql.Option.Call if self.cp=="C" else ql.Option.Put 
             payoff = ql.PlainVanillaPayoff(cp, self.strike_price)
