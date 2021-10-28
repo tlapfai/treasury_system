61c9f4a08519f934e7ea00b0efa888cdb112091f
Anthony Tam <40433247+tlapfai@users.noreply.github.com>
2021-10-28 18:06:13 +0800

    add 2 forms

--- swpm/views.py
+++ swpm/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import *
 from django.utils import timezone
+from .models import *
 
 # Create your views here.
 def index(request):
     mytime = timezone.now()
+    myform = CcyPairForm()
+    myFXOform = FXOForm()
     return render(request, "swpm/index.html", locals())
