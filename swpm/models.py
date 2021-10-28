07bc679a397abfdb2bd792f9c483db7662177e42
Anthony Tam <40433247+tlapfai@users.noreply.github.com>
2021-10-28 18:05:43 +0800

    add 2 forms

--- swpm/models.py
+++ swpm/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.db.models.deletion import CASCADE, DO_NOTHING
 from django.db.models.fields.related import ForeignKey
 from django.utils import timezone
+from django.forms import ModelForm, SelectDateWidget, DateInput
+from django import forms
 import QuantLib as ql
 
 FXO_TYPE = [("EUR","European"), 
@@ -137,3 +139,26 @@ class FXO(models.Model):
         r = self.ccy_pair.quote_ccy.fx_curve.all()[0].term_structure()
         process = ql.BlackScholesMertonProcess(spot_rate.handle(), q, r, v)
         return ql.AnalyticEuropeanEngine(process)
+
+class CcyPairForm(ModelForm):
+    class Meta:
+        model = CcyPair
+        fields = '__all__'
+        
+class FXOForm(ModelForm):
+    class Meta:
+        model = FXO
+        fields = ['trade_date', 'maturity_date', 'ccy_pair', 'strike_price', 'notional_1', 'notional_2', 'type', 'cp']
+        widgets = {
+            'trade_date': DateInput(attrs={'type': 'date'}),
+            'maturity_date': DateInput(attrs={'type': 'date'}),
+            }
+        labels = {
+            'cp': 'Call or Put',
+            }
+        help_texts = {
+            'ccy_pair': 'Choose a pair'
+            }
+    #def __init__(self, *args, **kwargs):
+    #    super().__init__(*args, **kwargs)
+    #    self.fields['trade_date'].widget.attrs.update({'type': 'date'})
