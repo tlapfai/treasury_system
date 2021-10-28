ed8646b4ac27aca872223e42b1e3e1ddec362a5d
Anthony Tam <40433247+tlapfai@users.noreply.github.com>
2021-10-28 18:39:43 +0800

    +pricing

--- swpm/views.py
+++ swpm/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 from django.http import *
 from django.utils import timezone
 from .models import *
+import datetime
 
-# Create your views here.
 def index(request):
     mytime = timezone.now()
     myform = CcyPairForm()
     myFXOform = FXOForm()
     return render(request, "swpm/index.html", locals())
+
+def pricing(request):
+    opt_class = FXO()
+    opt = FXOForm(request.POST, instance=opt_class)
+    opt_inst = opt.instrument()
+    engine = opt.make_pricing_engine(datetime.date.today())
+    
