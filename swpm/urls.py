7e38528857f88db66ebcbda894f85def03843699
Anthony Tam <40433247+tlapfai@users.noreply.github.com>
2021-10-29 18:00:52 +0800

    Update urls.py

--- swpm/urls.py
+++ swpm/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('pricing', views.pricing, name='pricing')
+    path('pricing', views.pricing, name='pricing'),
+    path('pricing/do', views.pricing, {'commit': True}, name='pricing-do'),
+    path('ccypair', views.save_ccypair, name='ccypair'), 
+    path('trade_list', views.trade_list, name='trade_list'),
 ]
