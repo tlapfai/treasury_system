c2049c5cfc84e9192c85ecaf7da770b5dbd8b132
Anthony Tam <40433247+tlapfai@users.noreply.github.com>
2021-11-05 17:47:07 +0800

    Update tests.py

--- swpm/tests.py
+++ swpm/tests.py
@@ -10,28 +10,26 @@ class FXOTestCase(TestCase):
         d = datetime.date(2021,10,28)
         md = datetime.date(2021,12,28)
 
-        cdr_us = Calendar.objects.create(name='UnitedStates')
-        cdr_eu = Calendar.objects.create(name='TARGET')
-        cdr_uk = Calendar.objects.create(name='UnitedKingdom')
-        cdr_hk = Calendar.objects.create(name='HongKong')
+        hongkong, _ = Calendar.objects.get_or_create(name="HongKong")
+        unitedstates, _ = Calendar.objects.get_or_create(name="UnitedStates")
+        target, _ = Calendar.objects.get_or_create(name="TARGET")
 
-        hkd = Ccy.objects.create(code="HKD", cdr=cdr_hk)
-        cnh = Ccy.objects.create(code="CNH", cdr=cdr_hk)
-        usd = Ccy.objects.create(code="USD", cdr=cdr_us)
-        eur = Ccy.objects.create(code="EUR", cdr=cdr_eu)
-        eurusd = CcyPair.objects.create(base_ccy=eur, quote_ccy=usd, cdr=cdr_us)
-        usdhkd = CcyPair.objects.create(base_ccy=usd, quote_ccy=hkd, cdr=cdr_hk)
+        hkd, _ = Ccy.objects.get_or_create(code="EUR", defaults={'cdr': hongkong})
+        eur, _ = Ccy.objects.get_or_create(code="EUR", defaults={'cdr': target})
+        usd, _ = Ccy.objects.get_or_create(code="USD", defaults={'cdr': unitedstates})
+        eurusd, _ = CcyPair.objects.get_or_create(name="EUR/USD", base_ccy=eur, quote_ccy=usd, defaults={'cdr': target})
+        usdhkd, _ = CcyPair.objects.get_or_create(name="USD/HKD", base_ccy=usd, quote_ccy=hkd, defaults={'cdr': hongkong})
 
         spot = FxSpotRateQuote.objects.create(ref_date=d, rate=1.098, ccy_pair=eurusd)
         vol = FXVolatility.objects.create(ref_date=d, vol=0.08, ccy_pair=eurusd)
-        r = RateQuote.objects.create(name='USD LIBOR 3M', rate=0.0021, tenor='3M', instrument='DEPO', ccy=usd, day_counter='A360')
+        r = RateQuote.objects.create(ref_date=d, name='USD LIBOR 6M', rate=0.0021, tenor='6M', instrument='DEPO', ccy=usd, day_counter='Actual360')
         rTS = IRTermStructure.objects.create(name='USD LIBOR', ref_date=d, as_fx_curve=usd)
         rTS.rates.add(r)
-        q = RateQuote.objects.create(name='EUR FX 3M', rate=-0.002, tenor='3M', instrument='DEPO', ccy=usd, day_counter='A365')
+        q = RateQuote.objects.create(ref_date=d, name='EUR FX 3M', rate=-0.002, tenor='3M', instrument='DEPO', ccy=usd, day_counter='Actual365Fixed')
         qTS = IRTermStructure.objects.create(name='EUR FX', ref_date=d, as_fx_curve=eur)
         qTS.rates.add(r)
         opt = FXO.objects.create(trade_date=d, maturity_date=md, ccy_pair=eurusd, 
-            strike_price=1.12, notional_1=100, notional_2=112, type='EUR', cp='C')
+            strike_price=1.12, notional_1=1000000, notional_2=1120000, type='EUR', cp='C')
 
     def test_npv(self):
         d = datetime.date(2021,10,28)
@@ -43,3 +41,7 @@ class FXOTestCase(TestCase):
         opt_inst.setPricingEngine(engine)
         print(f'NPV is {opt_inst.NPV()}')
         self.assertEqual(a.rate, 1.098)
+
+class RateFixingTestCase(TestCase):
+    def setUp(self):
+        pass
