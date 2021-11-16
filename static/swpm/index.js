fc89936236aaa2bfc8d8b2c7afc2689111d0d6a3
tlapfai <tlapfai@hotmail.com>
2021-11-16 23:14:48 +0800

    collect static

--- static/swpm/index.js
+++ static/swpm/index.js
@@ -5,12 +5,31 @@ document.addEventListener('DOMContentLoaded', function() {
         $('.ccypair button').toggle();
     });
 
+    $('#id_notional_1').change( () => {
+      if ( $.isNumeric($('#id_notional_2').val()) ) {
+        $('#id_strike_price').val( ($('#id_notional_2').val()/$('#id_notional_1').val()).toFixed(10) )
+		  }
+    });
+    $('#id_notional_2').change( () => {
+      if ( $.isNumeric($('#id_notional_1').val()) ) {
+        $('#id_strike_price').val( ($('#id_notional_2').val()/$('#id_notional_1').val()).toFixed(10) )
+		  }
+    });
+    $('#id_strike_price').change( () => {
+      if ( $.isNumeric($('#id_notional_1').val()) ) {
+        $('#id_notional_2').val( ($('#id_notional_1').val()*$('#id_strike_price').val()).toFixed(2) )
+		  }
+      else if ( $.isNumeric($('#id_notional_2').val()) ) {
+        $('#id_notional_1').val( ($('#id_notional_2').val()/$('#id_strike_price').val()).toFixed(2) )
+		  }
+    });
+
     document.querySelectorAll('legend')[0].addEventListener('click', () =>{
         const effective_date = new Date();
-		effective_date.setDate(effective_date.getDate()+2);
+		    effective_date.setDate(effective_date.getDate()+2);
         var oneyear = new Date();
         oneyear.setFullYear(oneyear.getFullYear()+1);
-		oneyear.setDate(oneyear.getDate()+2);
+		    oneyear.setDate(oneyear.getDate()+2);
         document.getElementById('id_book').value = "FXO1"
         document.getElementById('id_counterparty').value = "HSBC";
         document.querySelectorAll('[id*="effective_date"]').forEach( d => {d.value = effective_date.toISOString().split('T')[0];} );
@@ -18,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelectorAll('[id*="notional"]').forEach( d => {d.value = '1000000';} );
         document.querySelectorAll('#id_form-0-fixed_rate').forEach( d => {d.value = '0.03';} );
         document.querySelectorAll('[id*="freq"]').forEach( d => {d.value = '3M';} );
-        document.getElementById('id_form-0-reset_freq').value = '';
+        document.querySelectorAll('#id_form-0-reset_freq').forEach( d => {d.value = '';} );
         document.querySelectorAll('[id*="day_counter"]').forEach( d => {d.value = 'Actual360';} );
         document.querySelectorAll('[id*="index"]')[0].value = '';
         document.querySelectorAll('[id*="index"]')[1].value = 'USD LIBOR 3M';
@@ -29,12 +48,22 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelectorAll('[id*="cp"]')[0].value = 'C';
         document.querySelectorAll('[id*="buy_sell"]')[0].value = 'B';
         document.querySelectorAll('[id*="type"]')[0].value = 'EUR';
-        document.querySelectorAll('[id*="ccy_pair"]')[0].value = 'EUR/USD';
+        document.querySelectorAll("id_ccy_pair").forEach( d => {d.value = 'EUR/USD';} );
     });
 	
 	$('.btn-primary').click( () => {
         $('.full-screen').show();
 	});
+
+  var triggerTabList = [].slice.call(document.querySelectorAll('#ticketTab a'))
+  triggerTabList.forEach(function (triggerEl) {
+    var tabTrigger = new bootstrap.Tab(triggerEl)
+
+    triggerEl.addEventListener('click', function (event) {
+      event.preventDefault()
+      tabTrigger.show()
+    })
+  })
 
     // function updateTextView(_obj){
         // var num = getNumber(_obj.val());
